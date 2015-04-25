1616ee897730ee21888e7768643a62d6b4a5c223
Yoonsung <lvev9925@naver.com>
2015-04-25 12:10:33 +0900

    (Functions) declare void argument function

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,18 @@
 fn main() {
-	let x: i32; //warning not used
+	print_number(5);
+	print_sum(10, 2);
+}
+
+fn print_sum(x: i32, y: i32) {
+	println!("sum is : {}", x);
+}
+
+fn print_number(x: i32) {
+	println!("x is: {}", x);
+}
+	
+fn variable_binding() {
+	//let x: i32; //warning not used
 	//println(!"The value of x is : {}", x); //error not initialized
     println!("Hello world!");
 }
